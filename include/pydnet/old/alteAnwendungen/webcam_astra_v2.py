5ec75a9545eba9e228c664d26040ba9ef015d120
Stephan Sandfuchs <stephan.sandfuchs@hs-bochum.de>
2022-06-15 09:17:06 +0200

    PydNet Node and first try of a deep mono node

---
+++ include/pydnet/old/alteAnwendungen/webcam_astra_v2.py
@@ -0,0 +1,243 @@
+#
+# MIT License
+#
+# Copyright (c) 2018 Matteo Poggi m.poggi@unibo.it
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import tensorflow as tf
+import sys
+import os
+import argparse
+import time
+import datetime
+from utils import *
+from pydnet_v2 import *
+
+from primesense import openni2
+from primesense import _openni2 as c_api
+import numpy as np
+import cv2
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as plt3D
+
+openni2_dist="/home/stephan/turtlebot2_ros_melodic/catkin_ws/src/astra_camera/include/openni2_redist/x64/"
+
+# forces tensorflow to run on CPU
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+parser = argparse.ArgumentParser(description='Argument parser')
+
+""" Arguments related to network architecture"""
+parser.add_argument('--width', dest='width', type=int, default=512, help='width of input images')
+parser.add_argument('--height', dest='height', type=int, default=256, help='height of input images')
+parser.add_argument('--resolution', dest='resolution', type=int, default=1, help='resolution [1:H, 2:Q, 3:E]')
+parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', type=str, default='checkpoint/IROS18/pydnet', help='checkpoint directory')
+
+args = parser.parse_args()
+
+def main(_):
+
+  with tf.Graph().as_default():
+    height = args.height
+    width = args.width
+    placeholders = {'im0':tf.compat.v1.placeholder(tf.float32,[None, None, None, 3], name='im0')}
+
+    with tf.compat.v1.variable_scope("model") as scope:
+      model = pydnet(placeholders)
+
+    init = tf.group(tf.compat.v1.global_variables_initializer(),
+                   tf.compat.v1.local_variables_initializer())
+
+    loader = tf.compat.v1.train.Saver()
+    saver = tf.compat.v1.train.Saver()
+    
+    openni2.initialize(openni2_dist)
+    if (openni2.is_initialized()):
+        print("OpenNI2 initialized")
+    else:
+        raise ValueError("OpenNI2 failed to initialize!!")
+    
+    ## Register the device
+    dev = openni2.Device.open_any()
+    
+    ## Create the streams stream
+    rgb_stream = dev.create_color_stream()
+    depth_stream = dev.create_depth_stream()
+    
+    ## Configure the depth_stream -- changes automatically based on bus speed
+    #print 'Depth video mode info', depth_stream.get_video_mode() # Checks depth video configuration
+    depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX=640, resolutionY=480, fps=30))
+    
+    ## Check and configure the mirroring -- default is True
+    ## Note: I disable mirroring
+    # print 'Mirroring info1', depth_stream.get_mirroring_enabled()
+    depth_stream.set_mirroring_enabled(False)
+    rgb_stream.set_mirroring_enabled(False)
+    
+    ## More infor on streams depth_ and rgb_
+    #help(depth_stream)
+    
+    
+    ## Start the streams
+    rgb_stream.start()
+    depth_stream.start()
+    
+    ## Synchronize the streams
+    dev.set_depth_color_sync_enabled(True) # synchronize the streams
+    
+    ## IMPORTANT: ALIGN DEPTH2RGB (depth wrapped to match rgb stream)
+    dev.set_image_registration_mode(openni2.IMAGE_REGISTRATION_DEPTH_TO_COLOR)
+    
+    def get_rgb():
+        """
+        Returns numpy 3L ndarray to represent the rgb image.
+        """
+        bgr   = np.fromstring(rgb_stream.read_frame().get_buffer_as_uint8(),dtype=np.uint8).reshape(480,640,3)#.reshape(240,320,3)
+        rgb   = cv2.cvtColor(bgr,cv2.COLOR_BGR2RGB)
+        return rgb
+    #get_rgb
+
+
+    def get_depth():
+        """
+        Returns numpy ndarrays representing the raw and ranged depth images.
+        Outputs:
+            dmap:= distancemap in mm, 1L ndarray, dtype=uint16, min=0, max=2**12-1
+            d4d := depth for dislay, 3L ndarray, dtype=uint8, min=0, max=255    
+        Note1: 
+            fromstring is faster than asarray or frombuffer
+        Note2:     
+            .reshape(120,160) #smaller image for faster response 
+                    OMAP/ARM default video configuration
+            .reshape(240,320) # Used to MATCH RGB Image (OMAP/ARM)
+                    Requires .set_video_mode
+        """
+        dmap = np.fromstring(depth_stream.read_frame().get_buffer_as_uint16(),dtype=np.uint16).reshape(480,640)  # Works & It's FAST
+        d4d = np.uint8(dmap.astype(float) *255/ 2**12-1) # Correct the range. Depth images are 12bits
+        d4d = 255 - cv2.cvtColor(d4d,cv2.COLOR_GRAY2RGB)
+        return dmap, d4d
+    #get_depth
+    
+    #cam = cv2.VideoCapture(0)
+
+    with tf.compat.v1.Session() as sess:
+        sess.run(init)
+        loader.restore(sess, args.checkpoint_dir)
+
+        
+        while True:
+          """
+          for i in range(4):
+            cam.grab()
+          ret_val, img = cam.read()
+          """
+          
+          img = get_rgb()
+          #DEPTH (For comparison)
+          dmm,d4d = get_depth()
+          d4d_color = cv2.resize(d4d, (width, height))#.astype(np.float32) / 255.
+          #d4d_color = cv2.resize(d4d, (640, 320))#.astype(np.float32) / 255.
+          #d4d_color = cv2.applyColorMap(d4d_color, cv2.COLORMAP_INFERNO)
+         
+          
+          img = cv2.resize(img, (width, height)).astype(np.float32) / 255.
+          img = np.expand_dims(img, 0)
+          start = time.time()
+          disp = sess.run(model.results[args.resolution-1], feed_dict={placeholders['im0']: img})
+          end = time.time()
+
+          disp_color = applyColorMap(disp[0,:,:,0]*20, 'gist_gray')*255.
+          dist_color = applyColorMap(disp[0,:,:,1]*20, 'gist_gray')*255.
+          
+          img_disp = img[0] * 255.
+          #print(np.array(img_disp).shape, np.array(dist_color).shape, np.array(d4d_color).shape) 
+          #toShow = (np.concatenate((img_disp, disp_color,  dist_color, d4d_color), 0)).astype(np.uint8)
+          #toShow = (np.concatenate((img_disp, disp_color, d4d_color), 0)).astype(np.uint8)
+          toShow = (np.concatenate((img_disp, dist_color, d4d_color), 0)).astype(np.uint8)
+          #toShow = (np.concatenate((img_disp, disp_color, dist_color), 0)).astype(np.uint8)
+
+
+
+
+          toShow = cv2.resize(toShow, (width/2, height))
+          
+          cv2.imshow('pydnet', toShow)
+          
+          dmm_pc = dmm/1000.0 #Scaling from mm to m
+          orig_shape = np.shape(dmm_pc)
+          
+          network_scaler = 255.0#/100.0
+          x_disp = range(0,np.shape(disp[0,:,:,0])[0])
+          y_disp = range(0,np.shape(disp[0,:,:,0])[1])
+          z_disp = disp[0,:,:,0] * network_scaler
+          
+          x_dist = range(0,np.shape(disp[0,:,:,1])[0])
+          y_dist = range(0,np.shape(disp[0,:,:,1])[1])
+          z_dist = disp[0,:,:,1] * network_scaler
+          
+          x_dm = range(0,np.shape(dmm)[0])
+          y_dm = range(0,np.shape(dmm)[1])
+          z_dm = dmm / 1000.0    #scale to m
+          
+          #print(z_disp)
+          #print(z_dist)
+          #print(z_dm)
+          
+          
+          
+          #print(np.shape(dmm_pc), np.shape(d4d), np.shape(disp))
+          
+          #print(disp[0, :,:,0])
+          #print(disp[0, :,:,1])
+          #disp_pc = cv2.resize(disp[0,:,:,0], (orig_shape)) *20.*255./100. #TODO: Check Factor
+          
+          #err = dmm_pc - disp_pc    
+          
+          
+          k = cv2.waitKey(1)         
+          if k == 1048603 or k == 27:
+              np.set_printoptions(threshold=sys.maxsize)
+              f = open("./testfile.txt", "w")
+              #f.write(np.array2string(z_dm))
+              #f.write(np.array2string(z_disp))
+              f.write(np.array2string(z_dist))
+              f.close()
+              break  # esc to quit
+          if k == 1048688:
+              cv2.waitKey(0) # 'p' to pause
+            
+
+          print("Time: " + str(end - start))
+          del img
+          del disp
+          del toShow
+          
+        # Release resources   
+        #cam.release()        
+        cv2.destroyAllWindows()
+        rgb_stream.stop()
+        depth_stream.stop()
+        openni2.unload()
+        print ("Terminated")
+
+if __name__ == '__main__':
+    tf.compat.v1.app.run()
---
+++ ros/src/DeepMonoNode.cc
@@ -0,0 +1,148 @@
+#include "DeepMonoNode.h"
+
+//Only on the n-th keyframe the stereo image will be used. In tracking no stereo information will be used.
+//The use stereo information in tracking set this value to 0. This leads to every keyframe getting stereo information
+//Other cases are not intended at the moment
+const int nStereoEnhanced_glob = 0;
+const int nKFEnhanced_glob = 4;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "DeepMonoNode");
+    ros::start();
+
+    if(argc > 1) {
+        ROS_WARN ("Arguments supplied via command line are neglected.");
+    }
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ros::NodeHandle node_handle;
+    image_transport::ImageTransport image_transport (node_handle);
+
+    //DeepMonoNode node (ORB_SLAM2::System::RGBD , node_handle, image_transport);
+    DeepMonoNode node (ORB_SLAM2::System::DEEP_MONOCULAR , node_handle, image_transport);
+    //DeepMonoNode node (ORB_SLAM2::System::MONOCULAR , node_handle, image_transport);
+
+    node.Init();
+
+    ros::spin();
+
+    ros::shutdown();
+
+    return 0;
+}
+
+
+DeepMonoNode::DeepMonoNode (ORB_SLAM2::System::eSensor sensor, ros::NodeHandle &node_handle, image_transport::ImageTransport &image_transport) : 
+                              Node (sensor, node_handle, image_transport) {
+  left_image_subscriber = image_transport::Subscriber(image_transport.subscribe("/camera/rgb/image_raw", 1, &DeepMonoNode::ImageCallback_Mono, this));
+  deep_depth_image_subscriber = image_transport::Subscriber(image_transport.subscribe("/depth_network_image", 1, &DeepMonoNode::DeepDepthCallback, this));
+  
+  network_task_publisher = image_transport::Publisher(image_transport.advertise("/depth_model_task_image", 1));  
+  heartbeat_publisher_ = node_handle.advertise<orb_slam_deep_depth_ros::heartbeat>("/DeepMono/heartbeat", 1000);
+
+  publishHeartBeat();
+
+  node_handle_.getParam(name_of_node_ + "/nKFEnhance", nKFEnhanced);
+  std::cout << "KF Enhanced set to: " << nKFEnhanced << std::endl;
+  node_handle_.getParam(name_of_node_ + "/nStereoDelay", nStereoEnhanced);
+  std::cout << "nStereoEnhanced set to: " << nStereoEnhanced << std::endl;
+  ROS_WARN("CAUTION: the parameter nKFEnhanced and nStereoEnhanced have no effect with the current implementation.");
+
+  publish_heartbeat_depth = true;
+  publish_heartbeat_mono = true;
+
+}
+
+
+DeepMonoNode::~DeepMonoNode () {
+}
+
+void DeepMonoNode::publishHeartBeat () {
+  //publish the heartbeat
+
+  if (publish_heartbeat_mono & publish_heartbeat_depth){
+    orb_slam_deep_depth_ros::heartbeat s_msg;
+    s_msg.header.stamp = ros::Time::now();
+    s_msg.mono_beat = true;
+    s_msg.depth_beat = false;
+    heartbeat_publisher_.publish(s_msg);    
+  }else{
+    ROS_WARN("Heartbeat can not be published.");
+  }
+
+}
+
+void DeepMonoNode::ImageCallback_Mono (const sensor_msgs::ImageConstPtr& msgRGB) {
+  static int frameCounter = 0;
+  
+  publish_heartbeat_mono = false;
+
+  cv_bridge::CvImageConstPtr cv_ptrRGB;
+  try {
+      cv_ptrRGB = cv_bridge::toCvShare(msgRGB);
+  } catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+  }
+  current_frame_time_ = msgRGB->header.stamp;
+
+  bool isEnhanced = (frameCounter++ % nStereoEnhanced == 0);
+
+  orb_slam_->TrackDeepMono(cv_ptrRGB->image, cv_ptrRGB->header.stamp.toSec(), isEnhanced);
+  std::cout << "Monocular Image was grabbed at: " << current_frame_time_.toSec() << std::endl;
+  
+  if (isEnhanced) {
+    network_task_publisher.publish(msgRGB);
+    cv_RBG_Matrix_enhancement = (cv_ptrRGB->image).clone();
+  }
+  Update();
+
+  publish_heartbeat_mono = true;
+  publishHeartBeat();
+}
+
+void DeepMonoNode::DeepDepthCallback (const sensor_msgs::ImageConstPtr& msgRGB) {
+
+  publish_heartbeat_depth = false;
+
+  cv_bridge::CvImageConstPtr cv_ptrDepth;
+  try {
+      cv_ptrDepth = cv_bridge::toCvShare(msgRGB);
+  } catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+  }
+  ROS_INFO("Depth image was grabbed at: %f", cv_ptrDepth->header.stamp.toSec());
+  orb_slam_->TrackDeepDepth(cv_RBG_Matrix_enhancement, cv_ptrDepth->image, cv_ptrDepth->header.stamp.toSec());
+  
+  publish_heartbeat_depth = true;
+  publishHeartBeat();
+}
+
+
+
+void DeepMonoNode::ImageCallback_RGBD (const sensor_msgs::ImageConstPtr& msgRGB, const sensor_msgs::ImageConstPtr& msgD) {
+  // Copy the ros image message to cv::Mat.
+  cv_bridge::CvImageConstPtr cv_ptrRGB;
+  try {
+      cv_ptrRGB = cv_bridge::toCvShare(msgRGB);
+  } catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+  }
+
+  cv_bridge::CvImageConstPtr cv_ptrD;
+  try {
+    cv_ptrD = cv_bridge::toCvShare(msgD);
+  } catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+  }
+
+  current_frame_time_ = msgRGB->header.stamp;
+  ROS_INFO("Callback called.");
+  orb_slam_->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec());
+  ROS_INFO("RGBD Image was grabbed at: %f", current_frame_time_.toSec());
+  Update ();
+}
